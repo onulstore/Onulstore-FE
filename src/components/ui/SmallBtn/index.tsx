4f7f536aa95ab667633f56bb95c8101650d3ba42
Hwa-J <hj1103.code@gmail.com>
2022-09-21 14:52:30 +0900

    #1 refactor: Small 버튼 type 수정

--- src/components/ui/SmallBtn/index.tsx
+++ src/components/ui/SmallBtn/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import * as S from "./style";
-type styleType = {
-  children: string
+type ButtonType = {
+  children: React.ReactNode
+  type?: 'submit' | 'reset' | 'button'
+  onClick?: () => void
   line?: boolean
 }
-function SmallBtn(props: styleType) {
+
+function SmallBtn(props: ButtonType) {
   return (
     <>
       <S.SmallBtn {...props}>
