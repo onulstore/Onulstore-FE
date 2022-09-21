4c40d680f61a5361f3290103397556e0dbc59902
Hwa-J <hj1103.code@gmail.com>
2022-09-22 04:04:43 +0900

    #1 design: Small, Large 버튼 사이즈 및 폰트 수정

--- src/components/ui/LargeBtn/style.ts
+++ src/components/ui/LargeBtn/style.ts
@@ -8,6 +8,7 @@ type styleType = {
 export const LargeBtn = styled.button<styleType>`
   width: 31.1rem;
   height: 6rem;
+  margin: 0.2rem;
   box-sizing: border-box;
   display: flex;
   justify-content: center;
@@ -19,7 +20,7 @@ export const LargeBtn = styled.button<styleType>`
   padding: 1.7rem 1.2rem;
   background: ${(props) => props.color || '#2F2F33'};
   line-height: 150%;
-
+  // 세로 길이 다른 버튼
   ${(props) => props.middleHeight && css`
     height: 5rem;
     `}
@@ -29,13 +30,16 @@ export const TextContainer = styled.div<styleType>`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-family: 'MontserratSemiBold';
+  font-family: 'Montserrat';
   font-size: 1.8rem;
+  font-weight: 600;
+  // 세로 길이 다른 버튼 폰트 조정
   ${(props) => ( props.middleHeight || props.google ) && css`
-    font-family: 'PretendardBold';
+    font-family: 'Pretendard';
     font-size: 1.5rem;
+    font-weight: 700;
     `}
-  & img {
+  & svg {
     margin: 0 .8rem;
   }
 `
--- src/components/ui/SmallBtn/style.ts
+++ src/components/ui/SmallBtn/style.ts
@@ -6,6 +6,8 @@ type styleType = {
 export const SmallBtn = styled.button<styleType>`
   width: 9.6rem;
   height: 2.8rem;
+  margin: 0.2rem;
+  padding-top: 0.2rem;
   box-sizing: border-box;
   display: flex;
   justify-content: center;
@@ -16,9 +18,8 @@ export const SmallBtn = styled.button<styleType>`
   outline: none;
   background-color: #2F2F33;
   word-break: keep-all;
-  font-family: 'PretendardRegular';
   font-size: 1.2rem;
-
+  // line 들어간 버튼
   ${(props) => props.line && css`
     height: 2.2rem;
     border: .1rem solid #A9A9A9;
