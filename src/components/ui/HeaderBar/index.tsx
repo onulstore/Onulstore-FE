20c918cdeb04aaf624cf44f3f24efe29a7fa679d
Hwa-J <hj1103.code@gmail.com>
2022-09-21 21:23:27 +0900

    #1 feat: Header Bar 제작

---
+++ src/components/ui/HeaderBar/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { LogoIcon, SearchIcon, ShoppingBagIcon } from 'components/Icons'
+import { useLocation, useNavigate } from 'react-router-dom'
+import * as S from './style'
+
+function HeaderBar() {
+  const navigate = useNavigate()
+  const pathname = useLocation().pathname
+
+  return (
+    <S.HeaderBar>
+    {'/' === pathname &&
+      <S.HeaderLeft onClick={() => navigate("/")}>
+        <LogoIcon/>
+      </S.HeaderLeft>
+    }
+      <S.HeaderUl>
+        <S.HeaderLi>
+          <SearchIcon/>
+        </S.HeaderLi>
+        <S.HeaderLi onClick={() => navigate("/cart")}>
+          <ShoppingBagIcon/>
+        </S.HeaderLi>
+      </S.HeaderUl>
+    </S.HeaderBar>
+  )
+}
+
+export default HeaderBar
---
+++ src/components/ui/HeaderBar/style.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const HeaderBar = styled.nav`
+  width: 100%;
+  height: 5rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+`
+
+export const HeaderLeft = styled.div`
+  width: 13rem;
+  height: 2.35rem;
+  margin-left: 1.5rem;
+  cursor: pointer;
+`
+
+export const HeaderUl = styled.ul`
+  display: flex;
+  list-style: none;
+  align-items: center;
+`
+export const HeaderLi = styled.li`
+  margin-right: 1.5rem;
+  cursor: pointer;
+`;
