4a380ffa635cade33a398a10b64ae5dfd28e2ce5
Hwa-J <hj1103.code@gmail.com>
2022-09-21 15:51:13 +0900

    #1 design: Small 버튼 크기 및 폰트 수정

--- src/components/ui/SmallBtn/style.ts
+++ src/components/ui/SmallBtn/style.ts
@@ -4,26 +4,25 @@ type styleType = {
   line?: boolean
 }
 export const SmallBtn = styled.button<styleType>`
-  width: 6.6rem;
-  height: 1.75rem;
+  width: 9.6rem;
+  height: 2.8rem;
   box-sizing: border-box;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   border: none;
-  border-radius: 1.25rem;
-  padding: 0.3rem 1rem;
+  border-radius: 2rem;
   color: #fff;
   outline: none;
   cursor: pointer;
   background-color: #2F2F33;
   word-break: keep-all;
-  vertical-align: middle;
+  font-family: 'PretendardRegular';
+  font-size: 1.2rem;
+  font-weight: 600;
 
   ${(props) => props.line && css`
-    height: 1.37rem;
-    padding: .5rem .8rem;
-    border: .06rem solid #A9A9A9;
+    height: 2.2rem;
+    border: .1rem solid #A9A9A9;
     `}
 `
