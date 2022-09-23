ddf5220535af6fec28cab58905344de1f5ddfe62
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-09-23 13:08:04 +0900

    feat: 아이템페이지 및 상세페이지 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
+// STYLED-COMPONENTS
 import GlobalStyles from './style/global';
 import theme from '../src/style/theme';
 import { ThemeProvider } from 'styled-components';
+import * as S from './components/layout/index';
+// PAGES
 import Home from './pages/Home';
+import Items from './pages/Items';
+import ItemDetail from 'pages/ItemDetail';
+//COMPONENTS
+import HeaderBar from 'components/ui/navbar/HeaderBar';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes>
-        <Route index element={<Home />} />
-      </Routes>
+      <S.Layout>
+        <HeaderBar />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/items" element={<Items />} />
+          <Route path="/items/:id" element={<ItemDetail />} />
+        </Routes>
+      </S.Layout>
     </ThemeProvider>
   );
 };
