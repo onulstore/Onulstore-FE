4baf18f68095d6ab7d67478a91008c1e04a336bf
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-09-23 13:31:52 +0900

    feat:전체레이아웃 만듬 및 모바일 데스크탑에 따라 중앙배치 여부 결정

---
+++ src/components/layout/index.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  min-height: 81.2rem;
+  width: 37.5rem;
+
+  @media (min-width: 1140px) {
+    margin: auto;
+  }
+`;
