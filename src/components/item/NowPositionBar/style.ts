07a7ae25634b75a54589ab84175145f508fb52d4
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-09-25 19:10:13 +0900

    feat: 상품페이지에서 현제 위치를 표시해 주는 컴포넌트 및 style 추가

---
+++ src/components/item/NowPositionBar/style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const NowPositionBarContainer = styled.div`
+  height: 4.4rem;
+
+  display: flex;
+  align-items: center;
+
+  padding: 1rem 2.4rem;
+  gap: 1.4rem;
+
+  border-bottom: 1px solid rgba(214, 214, 214, 0.5);
+  .text {
+    font-size: 1.6rem;
+    font-weight: 500;
+    color: #a9a9a9;
+    box-sizing: border-box;
+  }
+`;
