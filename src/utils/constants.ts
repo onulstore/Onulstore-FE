fc1d8c16ba40dc6c4975aeabca23b5ea0a4a9e1d
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-09-25 19:10:46 +0900

    feat: filterBox에 필요한 text 배열화

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,6 @@
-export const pageName: {[key: string]: string}= {
+import { nanoid } from 'nanoid';
+
+export const pageName: { [key: string]: string } = {
   // key 이름은 페이지의 path가 정확히 정해지면 수정할 예정
   '/login': 'LOGIN',
   '/join': 'JOIN',
@@ -8,5 +10,29 @@ export const pageName: {[key: string]: string}= {
   '/detail': '',
   '/cart': 'CART',
   '/review': '리뷰쓰기',
-  '/qna': '문의하기'
+  '/qna': '문의하기',
+};
+
+export interface IFilterBarTitle {
+  id: string;
+  title: string;
 }
+
+export const FilterBarTitle: IFilterBarTitle[] = [
+  {
+    id: nanoid(),
+    title: '최신순',
+  },
+  {
+    id: nanoid(),
+    title: '판매순',
+  },
+  {
+    id: nanoid(),
+    title: '리뷰순',
+  },
+  {
+    id: nanoid(),
+    title: '인기순',
+  },
+];
