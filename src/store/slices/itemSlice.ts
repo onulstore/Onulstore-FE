55412d9bd6460a1b279d5f8d5c8fdf6d243e486f
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-09-25 19:07:48 +0900

    feat: 비동기 로직을 다루는 extraReucers 추가 및 관련 로직 타입 추가

--- src/store/slices/itemSlice.ts
+++ src/store/slices/itemSlice.ts
@@ -1,13 +1,33 @@
-import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+//API
+import { getItem } from 'utils/Api/dumy/dumyApi';
 
-interface ItemType {
+interface StateType {
   isShowLargeCategoryModal: Boolean;
   LargeCategoryTitle: string;
+  isLoding: boolean;
+  allItemList: any[];
+  error: string;
 }
 
-const initialState: ItemType = {
-  isShowLargeCategoryModal: true,
+export interface AllItemList {
+  productName: string;
+  content: string;
+  largeCategoryCode: string;
+  smallCategoryCode: string;
+  price: number;
+  quantity: number;
+  purchaseCount: number;
+  productStatus: string;
+  id: string;
+}
+
+const initialState: StateType = {
+  isShowLargeCategoryModal: false,
   LargeCategoryTitle: '',
+  isLoding: false,
+  allItemList: [],
+  error: '요청실패!',
 };
 
 const itemSlice = createSlice({
@@ -17,6 +37,19 @@ const itemSlice = createSlice({
     largeCategoryModalHandler: (state) => {
       state.isShowLargeCategoryModal = !state.isShowLargeCategoryModal;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getItem.pending, (state) => {
+      state.isLoding = !state.isLoding;
+    });
+    builder.addCase(getItem.fulfilled, (state, action: PayloadAction<AllItemList[]>) => {
+      state.isLoding = !state.isLoding;
+      state.allItemList = action.payload;
+    });
+    builder.addCase(getItem.rejected, (state) => {
+      state.isLoding = !state.isLoding;
+      console.log(state.error);
+    });
   },
 });
 
