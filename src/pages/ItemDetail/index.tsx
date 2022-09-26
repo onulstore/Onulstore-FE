b7489ca4a2d5a1b66b6ad48d72bf38f52068d767
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-09-26 14:19:32 +0900

    refactor: 컴포넌트명 변경

--- src/pages/ItemDetail/index.tsx
+++ src/pages/ItemDetail/index.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { headerBarHandler } from 'store/slices/itemSlice';
+import { useAppDispatch } from 'store/hooks/index';
+const ItemDetail = () => {
+  const { state, pathname } = useLocation();
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(headerBarHandler());
 
-function ItemDetail() {
-  return <div>ItemDetail</div>;
-}
+    return () => {
+      dispatch(headerBarHandler());
+    };
+  }, []);
+  return <>{state.id}</>;
+};
 
 export default ItemDetail;
