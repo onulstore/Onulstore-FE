11f8a73e2058f0208f4211a80370bdb5b2090d4d
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-09-26 14:20:48 +0900

    feat: HeaderCenter를 숨기는 데이터 추가

--- src/store/hooks/index.ts
+++ src/store/hooks/index.ts
@@ -5,6 +5,8 @@ export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const useItemSlice = () => {
-  const { isShowLargeCategoryModal, allItemList } = useAppSelector((state) => state.item);
-  return { isShowLargeCategoryModal, allItemList };
+  const { isShowLargeCategoryModal, allItemList, isDetailPage } = useAppSelector(
+    (state) => state.item,
+  );
+  return { isShowLargeCategoryModal, allItemList, isDetailPage };
 };
