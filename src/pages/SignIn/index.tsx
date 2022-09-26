8d20d41e604033ddfbd0ece20d2a549787546f8e
k-m-jin <mel917@naver.com>
2022-09-26 18:48:56 +0900

    feat: Pages SignIn 추가

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,21 @@
+import SignInForm from 'components/user/SignInForm';
 import React from 'react';
+import * as S from './style';
+import { LogoIcon } from 'components/Icons';
 
 function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <S.Greeting>
+        <LogoIcon color={'#2F2F33'} width={'115'} height={'19.78'} />
+        <S.Text>
+          <div className="greeting">WECOME TO</div>
+          <div className="greeting"> ONUL STORE</div>
+        </S.Text>
+      </S.Greeting>
+      <SignInForm />
+    </div>
+  );
 }
 
 export default SignIn;
--- src/pages/SignIn/style.ts
+++ src/pages/SignIn/style.ts
@@ -1,1 +1,25 @@
 import styled from 'styled-components';
+
+export const Greeting = styled.div`
+  margin-left: 3.7rem;
+`;
+export const Text = styled.div`
+  margin-top: 1.23rem;
+  margin-bottom: 3.7rem;
+  .greeting {
+    font-family: Montserrat;
+    color: #2f2f33;
+    font-style: normal;
+    letter-spacing: 0.005em;
+    &:first-child {
+      font-weight: 400;
+      font-size: 20px;
+      line-height: 24px;
+    }
+    &:last-child {
+      font-weight: 500;
+      font-size: 34px;
+      line-height: 40px;
+    }
+  }
+`;
