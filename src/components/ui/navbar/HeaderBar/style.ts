d2e46f400634be1bc7ed2bc760881a49bf6d6cfd
Hwa-J <hj1103.code@gmail.com>
2022-09-27 19:41:28 +0900

    #11 design: HeaderBar 왼쪽 아이콘 위치 수정 & 가운데 영역 위치 수정

--- src/components/ui/navbar/HeaderBar/style.ts
+++ src/components/ui/navbar/HeaderBar/style.ts
@@ -11,17 +11,18 @@ export const HeaderBar = styled.nav`
 `;
 
 export const HeaderLeft = styled.div`
-  width: 13rem;
   height: 2.35rem;
-  cursor: pointer;
   & svg {
+    cursor: pointer;
+    border-color: red;
     margin-left: 2rem;
   }
 `;
 
 export const HeaderCenter = styled.div`
+  width: auto;
   text-align: center;
-  position: absolute;
+  position: relative;
   left: 0;
   right: 0;
   color: #26252a;
@@ -29,7 +30,13 @@ export const HeaderCenter = styled.div`
   font-size: 2rem;
   font-weight: 600;
   .Catagory {
-    margin-right: 10px;
+    cursor: pointer;
+  }
+  & svg {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto 10px;
   }
 `;
 
