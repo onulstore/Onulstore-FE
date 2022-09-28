77f6d3f674744a2825f8bb525eb67e0600313cda
Hwa-J <hj1103.code@gmail.com>
2022-09-29 02:34:52 +0900

    #19 design: TodayTimer 컴포넌트 스타일링

--- src/components/home/TodayTimer/index.tsx
+++ src/components/home/TodayTimer/index.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { useTodayCountdown } from 'store/hooks';
+import * as S from './style';
 
 interface ShowCounterProps {
   hours: string;
   minutes: string;
   seconds: string;
+  fontSize?: number;
 }
-const ShowCounter = ({ hours, minutes, seconds }: ShowCounterProps) => {
-  return (
-    <div>
-      <div>{`${hours}:${minutes}:${seconds}`}</div>
-    </div>
-  );
+interface TodayTimerProps {
+  tomorrow: number[];
+  fontSize?: number;
+}
+
+const ShowCounter = ({ hours, minutes, seconds, fontSize }: ShowCounterProps) => {
+  return <S.ShowCounter fontSize={fontSize}>{`${hours}:${minutes}:${seconds}`}</S.ShowCounter>;
 };
 
-function TodayTimer({ tomorrow }: any) {
+function TodayTimer({ tomorrow, fontSize }: TodayTimerProps) {
   const [hours, minutes, seconds] = useTodayCountdown(tomorrow);
 
   return (
     <ShowCounter
+      fontSize={fontSize}
       hours={String(hours).padStart(2, '0')}
       minutes={String(minutes).padStart(2, '0')}
       seconds={String(seconds).padStart(2, '0')}
