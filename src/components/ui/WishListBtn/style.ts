d1c6169a13457409753d8e8908ed7606f31810c2
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-09-28 18:20:58 +0900

    feat: 장바구니 버튼 구현

---
+++ src/components/ui/WishListBtn/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+export const WishListBtnContainer = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 8.8rem;
+  height: 7.1rem;
+
+  background: #d86145;
+
+  outline: none;
+  border: none;
+`;
