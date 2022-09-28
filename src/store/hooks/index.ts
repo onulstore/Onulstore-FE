8ef1744a4ecbfb2b2ee957d653da8de5bf6d7b8b
Hwa-J <hj1103.code@gmail.com>
2022-09-28 17:57:59 +0900

    #19 feat: useTodayCountdown 커스텀훅 생성

--- src/store/hooks/index.ts
+++ src/store/hooks/index.ts
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../index';
 
@@ -9,4 +10,30 @@ export const useItemSlice = () => {
     (state) => state.item,
   );
   return { isShowLargeCategoryModal, allItemList, isDetailPage };
+};
+
+export const useTodayCountdown = (tomorrow: number[]) => {
+  const [year, month, date] = tomorrow;
+  // 년, 월, 일 number 값을 new Date의 setting 값으로 입력해야 해당 날짜의 정각으로 설정됨
+  const countDownDate = new Date(year, month, date).getTime();
+
+  const [countdown, setCountdown] = useState(countDownDate - new Date().getTime());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown(countDownDate - new Date().getTime());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [countDownDate]);
+
+  return getReturnValues(countdown);
+};
+
+const getReturnValues = (countdown: number) => {
+  const hours = Math.floor((countdown / (60 * 60 * 1000)) % 24);
+  const minutes = Math.floor((countdown / (60 * 1000)) % 60);
+  const seconds = Math.floor((countdown / 1000) % 60);
+
+  return [hours, minutes, seconds];
 };
