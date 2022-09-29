0477c0220789a8bfcb099e2c3c12d488a259e8cd
Hwa-J <hj1103.code@gmail.com>
2022-09-29 14:50:45 +0900

    #19 rename: timeHooks.ts -> useTodayCountdown.ts 파일명 변경

--- src/components/home/TodayTimer/index.tsx
+++ src/components/home/TodayTimer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTodayCountdown } from 'store/hooks/timeHooks';
+import { useTodayCountdown } from 'store/hooks/useTodayCountdown';
 import * as S from './style';
 
 interface ShowCounterProps {
