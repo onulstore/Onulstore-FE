6c07a3f1613b52215201c1e0d9c4322c4ce4c15f
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-10-01 19:13:52 +0900

    design:도움말 페이지 css작업

--- src/pages/ItemDetail/DetailInfo/OrderGuide/style.ts
+++ src/pages/ItemDetail/DetailInfo/OrderGuide/style.ts
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 export const OrderGuideContainer = styled.div`
+  min-height: 66.1rem;
   padding: 3rem 3rem 3.9rem 2.5rem;
   box-sizing: border-box;
+  margin-bottom: 4rem;
   .order-guide-header {
     font-weight: 600;
-    font-size: 20px;
-    line-height: 24px;
+    font-size: 2rem;
+    line-height: 2.4rem;
     color: #2f2f33;
+    padding: 3rem 0 1.9rem 0;
+    box-sizing: border-box;
   }
 `;
