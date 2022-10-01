eeafcd54f13e5ca603e91615af428cbdff0d63f0
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-10-01 19:23:13 +0900

    design:도움말 페이지에 title 부분 스타일링

--- src/components/itemDetail/OrderGuide/OrderGuideTitle/style.ts
+++ src/components/itemDetail/OrderGuide/OrderGuideTitle/style.ts
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
-export const OrderGuideTitleContainer = styled.div`
-  .test-container {
+
+interface Props {
+  changeArrowIcon: any;
+}
+export const OrderGuideTitleContainer = styled.div<Props>`
+  .header-container {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    border-bottom: 2px solid #d6d6d6;
+    border-bottom: ${({ changeArrowIcon }) => (changeArrowIcon ? 'none' : '2px solid #d6d6d6')};
     padding: 20px 14px;
     box-sizing: border-box;
 
-    .test-text {
+    .header-title {
       font-weight: 600;
       font-size: 1.6rem;
       line-height: 1.9rem;
