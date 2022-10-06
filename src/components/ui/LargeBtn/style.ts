a2c8b2409f1661bb2188e38a00b94a706c7fbad8
Hwa-J <hj1103.code@gmail.com>
2022-10-07 02:14:11 +0900

    #56 refactor: LargeBtn 컴포넌트 border-radius 조절 가능하도록 수정

--- src/components/ui/LargeBtn/style.ts
+++ src/components/ui/LargeBtn/style.ts
@@ -1,28 +1,28 @@
-import styled, {css} from "styled-components";
+import styled, { css } from 'styled-components';
 
 type styleType = {
-  color?: string
-  // middleHeight?: boolean
-  font?: boolean
-  width?: number
-  height?: number
-}
+  color?: string;
+  font?: boolean;
+  width?: number;
+  height?: number;
+  borderRadius?: number;
+};
 export const LargeBtn = styled.button<styleType>`
-  width: ${({width = 31.1}) => width}rem;
-  height: ${({height = 6}) => height}rem;
+  width: ${({ width }) => width || 31.1}rem;
+  height: ${({ height }) => height || 6}rem;
   margin: 0.2rem;
   box-sizing: border-box;
   display: flex;
   justify-content: center;
   align-items: center;
   border: none;
-  border-radius: 0.5rem;
-  color: ${(props) => props.color === '#F5F5F5' ? '#505050' : '#fff'};
+  border-radius: ${({ borderRadius }) => borderRadius}rem;
+  color: ${({ color }) => (color === '#F5F5F5' ? '#505050' : '#fff')};
   outline: none;
   padding: 1.7rem 1.2rem;
   background: ${(props) => props.color || '#2F2F33'};
   line-height: 150%;
-`
+`;
 export const TextContainer = styled.div<styleType>`
   display: flex;
   justify-content: center;
@@ -31,12 +31,14 @@ export const TextContainer = styled.div<styleType>`
   font-size: 1.8rem;
   font-weight: 600;
   // 작은 폰트 조정
-  ${(props) =>  props.font  && css`
-    font-family: 'Pretendard';
-    font-size: 1.5rem;
-    font-weight: 700;
+  ${(props) =>
+    props.font &&
+    css`
+      font-family: 'Pretendard';
+      font-size: 1.5rem;
+      font-weight: 700;
     `}
   & svg {
-    margin: 0 .8rem;
+    margin: 0 0.8rem;
   }
-`
+`;
