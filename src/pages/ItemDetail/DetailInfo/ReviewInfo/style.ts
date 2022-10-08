d66bf0fd59f75633307db4c62d2a05aaefce5c02
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-10-09 06:29:14 +0900

    desing:리뷰 등록시 사용되는 컴포넌트 관련 여백 설정

--- src/pages/ItemDetail/DetailInfo/ReviewInfo/style.ts
+++ src/pages/ItemDetail/DetailInfo/ReviewInfo/style.ts
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 export const ReviewInfoContainer = styled.div`
+  position: relative;
+
   padding: 3rem 3rem 3.5rem 2.5rem;
   box-sizing: border-box;
 
+  min-height: 53.6rem;
+  overflow: scroll;
+  ::-webkit-scrollbar {
+    display: hidden;
+    width: 0;
+  }
   .review-header {
     display: flex;
     justify-content: space-between;
@@ -21,6 +29,27 @@ export const ReviewInfoContainer = styled.div`
       .pencil-icon {
         padding-left: 1rem;
         box-sizing: border-box;
+      }
+    }
+  }
+
+  .none-msg {
+    position: absolute;
+    top: 12.2rem;
+    left: 11.1rem;
+
+    font-weight: 600;
+    font-size: 1.6rem;
+    line-height: 1.9rem;
+    color: #a9a9a9;
+  }
+  .represent-img {
+    display: flex;
+    margin: 3.4rem 0;
+    .item {
+      width: 10.6rem;
+      &:nth-child(2) {
+        margin: 0 0.8rem;
       }
     }
   }
