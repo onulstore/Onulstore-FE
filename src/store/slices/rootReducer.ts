24f5db625a23ac47049e84c74acf7660b982a421
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-10-10 07:12:47 +0900

    feat: QnA슬라이스 추가

--- src/store/slices/rootReducer.ts
+++ src/store/slices/rootReducer.ts
@@ -1,9 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import item from './itemSlice';
 import review from './reviewSlice';
+import QnA from './QnASlice';
+
 const rootReducer = combineReducers({
   item,
   review,
+  QnA,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
