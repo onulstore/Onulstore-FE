d3e0c051026447c23c3eb930024049a06bb04701
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-10-10 07:06:22 +0900

    feat: QnA페이지 컴포넌트 추가 및 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import Items from 'pages/Items/index';
 import ItemDetail from 'pages/ItemDetail';
 import ItemInfo from 'pages/ItemDetail/DetailInfo/ItemInfo';
 import Payment from 'pages/Payment';
+import QnA from 'pages/QnA';
 import ReviewInfo from 'pages/ItemDetail/DetailInfo/ReviewInfo';
 import QandAInfo from 'pages/ItemDetail/DetailInfo/Q&AInfo';
 import OrderGuide from 'pages/ItemDetail/DetailInfo/OrderGuide';
@@ -19,6 +20,8 @@ import SimilarItem from 'pages/ItemDetail/DetailInfo/SimilarItem';
 import SignUp from 'pages/SignUp';
 import SignIn from 'pages/SignIn';
 import Review from 'pages/Review';
+
+import WishList from 'pages/WishList';
 //COMPONENTS
 import HeaderBar from 'components/ui/navbar/HeaderBar';
 import LargeCategoryModal from 'components/item/LargeCategoryModal';
@@ -45,10 +48,12 @@ const App = () => {
           </Route>
           <Route path="/payment" element={<Payment />} />
           <Route path="/review" element={<Review />} />
+          <Route path="qna" element={<QnA />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/login" element={<SignIn />} />
           <Route path="/non-member" element={<NonMember />} />
+          <Route path="/wish-list" element={<WishList />} />
         </Routes>
         <BottomTabBar />
       </S.Layout>
