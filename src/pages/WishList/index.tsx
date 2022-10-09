9e22aa307c64d6aa964d70e15af8df041883950e
Jung-Hwan-Kim-97 <blacksoy0516@gmail.com>
2022-10-10 07:14:49 +0900

    test: 장바구니 로직 구현을 위한 로직 테스트 시도

--- src/pages/WishList/index.tsx
+++ src/pages/WishList/index.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function WishList() {
-  return <div>WishList</div>;
-}
+const WishList = () => {
+  const [testArr, setTestArr] = useState<any>([]);
+  const [testNum, setTestNum] = useState(0);
+  let i = 3800;
+
+  const plusHandler = () => {
+    setTestArr([...testArr, i]);
+
+    const test =
+      testArr.reduce((acc: number, cur: number) => {
+        return acc + cur;
+      }, 3800) + 800;
+    setTestNum(test);
+  };
+
+  const minusHandler = () => {
+    if (testArr.length < 1) {
+      alert('한개이상 구매해야합니다');
+      return;
+    }
+    setTestArr(testArr.splice(1, 2));
+
+    setTestNum(
+      testArr.reduce((acc: number, cur: number) => {
+        return acc + cur;
+      }, 3800) + 800,
+    );
+  };
+  console.log(testArr);
+  console.log(testNum);
+  return (
+    <div>
+      <input type="number" />
+      <button onClick={plusHandler}>+</button>
+      <button onClick={minusHandler}>-</button>
+    </div>
+  );
+};
 
 export default WishList;
