a66492beeca7a7fb401f68fcd88f3ffa26ad6778
Hwa-J <hj1103.code@gmail.com>
2022-10-11 02:10:20 +0900

    #75 feat: SearchedKeywords 컴포넌트 제작

---
+++ src/components/search/SearchedKeywords/index.tsx
@@ -0,0 +1,62 @@
+import { dividerClasses } from '@mui/material';
+import { CloseIcon } from 'components/Icons';
+import React from 'react';
+import theme from 'style/theme';
+import * as S from './style';
+
+const searchHandler = (searchKeyword: string) => {
+  console.log(searchKeyword);
+};
+const deleteHandler = (searchKeywordId: string) => {
+  console.log(searchKeywordId);
+};
+const allDeleteHandler = (data: any) => {
+  console.log(data);
+};
+
+function SearchedKeywords({ title, data, recent }: any) {
+  return (
+    <S.RecentSearch>
+      <div className="wrapper">
+        <h1>{title}</h1>
+        {recent && (
+          <button
+            onClick={() => {
+              allDeleteHandler(data);
+            }}
+          >
+            모두 지우기
+          </button>
+        )}
+      </div>
+      <ul>
+        {data ? (
+          data.map((keyword: string, index: number) => (
+            <li key={index}>
+              <span
+                onClick={() => {
+                  searchHandler(keyword);
+                }}
+              >
+                {keyword}
+              </span>
+              {recent && (
+                <button
+                  onClick={() => {
+                    deleteHandler(keyword);
+                  }}
+                >
+                  <CloseIcon width="1rem" height="1rem" color={theme.palette.midGrey} />
+                </button>
+              )}
+            </li>
+          ))
+        ) : (
+          <div className="empty">{title}가 없습니다.</div>
+        )}
+      </ul>
+    </S.RecentSearch>
+  );
+}
+
+export default SearchedKeywords;
