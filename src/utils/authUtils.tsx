aaa675b50f3860ef8faa161a4e13dd6e78822687
k-m-jin <kmjin1105@gmail.com>
2022-10-12 02:11:49 +0900

    #68 axios interceoptor

--- src/utils/authUtils.tsx
+++ src/utils/authUtils.tsx
@@ -7,7 +7,7 @@ export const authCheck = async (token: string) => {
   const navigate = useNavigate();
 
   // 페이지에 들어올때 쿠키로 사용자 체크
-  // const token = cookies.accessToken; // 쿠키에서 accessToken 를 꺼내기
+  // const token = cookies.accessToken;
   console.log(token);
   const res = await api({
     url: 'members',
@@ -24,11 +24,25 @@ export const authCheck = async (token: string) => {
   // }
 };
 
+export const refresh = async () => {
+  const [cookies, setCookie, removeCookie] = useCookies();
+  const response = await api({
+    url: 'auth/refresh',
+    method: 'POST',
+    data: {
+      accessToken: cookies.accessToken,
+      refreshToken: cookies.refreshToken,
+    },
+  });
+  console.log('AuthService: refresh response is', response.data.accessToken);
+  return response.data.accessToken;
+};
+
 export const logOut = () => {
   const [cookies, setCookie, removeCookie] = useCookies();
   const navigate = useNavigate();
 
-  removeCookie('accessToken'); // 쿠키를 삭제
-  removeCookie('refreshToken'); // 쿠키를 삭제
-  navigate(0); // 메인 페이지로 이동
+  removeCookie('accessToken');
+  removeCookie('refreshToken');
+  navigate(0); // 새로고침
 };
