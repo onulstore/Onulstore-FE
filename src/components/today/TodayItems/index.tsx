7713f8a9d121fff984afe4d801ed2bb6fb163690
Hwa-J <hj1103.code@gmail.com>
2022-10-13 04:48:28 +0900

    #91 feat: TodayItems 컴포넌트 제작

---
+++ src/components/today/TodayItems/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as S from './style';
+import TodayTimerProgressBar from 'components/home/TodayTimerProgressBar';
+import { SmallBtn } from 'components/ui/SmallBtn/style';
+import { SmallReviewOnIcon } from 'components/Icons';
+import theme from 'style/theme';
+import CurrentTime from 'components/home/CurrentTime';
+import { TodayDate } from 'components/home/Date';
+
+function TodayItems({ data }: any) {
+  const navigate = useNavigate();
+  return (
+    <S.TodayItems>
+      <h1>
+        <TodayDate fontWeight={500} fontSize={2.4} />
+        <span>Special price</span>
+      </h1>
+      <ul>
+        {data?.map((item: any) => (
+          <li key={item.id}>
+            <S.TodayItem>
+              <div className="img-container">
+                <img src={item.productImage[0].imageName} alt={item.productName} />
+                <div className="progress-bar">
+                  <TodayTimerProgressBar baseColor="transparent" />
+                </div>
+              </div>
+              <S.TodayItemInfo>
+                <div className="item-info">
+                  <h2 className="brand-name">{item.brand}</h2>
+                  <div className="star-ratings">
+                    {item.rating}
+                    <SmallReviewOnIcon color={theme.palette.midGrey} />
+                    <SmallReviewOnIcon color={theme.palette.midGrey} />
+                    <SmallReviewOnIcon color={theme.palette.midGrey} />
+                    <SmallReviewOnIcon color={theme.palette.midGrey} />
+                    <SmallReviewOnIcon color={theme.palette.midGrey} />
+                  </div>
+                </div>
+                <h3 className="item-name">{item.productName}</h3>
+
+                <div className="price-container">
+                  <div className="discount-container">
+                    <span className="prediscount">¥ {item.price.toLocaleString('jp-JP')}</span>
+                    <div className="discount-wrapper">
+                      <span className="discount">{item.discount}%</span>
+                      <span className="price">
+                        ¥ {(item.price * (100 - item.discount)).toLocaleString('jp-JP')}
+                      </span>
+                    </div>
+                  </div>
+                  <SmallBtn
+                    width={10.3}
+                    height={3.2}
+                    fontSize={1.6}
+                    onClick={() => navigate(`/items/${item.id}`)}
+                  >
+                    구매하기
+                  </SmallBtn>
+                </div>
+              </S.TodayItemInfo>
+            </S.TodayItem>
+          </li>
+        ))}
+      </ul>
+    </S.TodayItems>
+  );
+}
+
+export default TodayItems;
---
+++ src/components/today/TodayItems/style.ts
@@ -0,0 +1,88 @@
+import styled from 'styled-components';
+
+export const TodayItems = styled.section`
+  margin: 0 2rem;
+  h1 {
+    margin-top: 3.2rem;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+    font-size: 2.4rem;
+    color: ${({ theme }) => theme.palette.black};
+    span {
+      margin-left: 0.6rem;
+    }
+  }
+`;
+export const TodayItem = styled.div`
+  margin: 3.2rem 0 1.5rem;
+  .img-container {
+    position: relative;
+    width: 33.5rem;
+    height: 33.5rem;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .progress-bar {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
+`;
+
+export const TodayItemInfo = styled.div`
+  font-family: 'Montserrat';
+  color: ${({ theme }) => theme.palette.black};
+  & .item-info {
+    margin: 1.9rem 0 0.9rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h2 {
+      font-weight: 700;
+      font-size: 1.4rem;
+      line-height: 130%;
+    }
+  }
+  h3 {
+    font-weight: 700;
+    font-size: 2rem;
+    line-height: 130%;
+    margin: 0.2rem 0;
+  }
+  .price-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .discount-container {
+    display: flex;
+    flex-direction: column;
+  }
+  .prediscount {
+    margin: 1.2rem 0 0.8rem;
+    font-size: 1.2rem;
+    color: ${({ theme }) => theme.palette.midGrey};
+    text-decoration-line: line-through;
+  }
+  .price {
+    font-weight: 600;
+    font-size: 1.8rem;
+    line-height: 150%;
+    color: ${({ theme }) => theme.palette.black};
+  }
+  .discount-wrapper {
+    display: flex;
+    align-items: center;
+    font-weight: 600;
+    font-size: 2rem;
+  }
+  .discount {
+    margin-right: 0.8rem;
+    color: ${({ theme }) => theme.palette.main};
+  }
+`;
