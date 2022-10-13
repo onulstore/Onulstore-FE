4bebf644825a006316077c9c0b6942732375e0b6
Hwa-J <hj1103.code@gmail.com>
2022-10-14 01:03:54 +0900

    #98 Magazine 페이지 제작

---
+++ src/pages/WeLike/Magazine/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TestMagazine } from 'components/weLike/MagazineList';
+
+function Magazine() {
+  return (
+    <div>
+      <TestMagazine />
+    </div>
+  );
+}
+
+export default Magazine;
