d57063f5e64aa32e511d39154fe1d32399fda640
Hwa-J <hj1103.code@gmail.com>
2022-10-14 04:10:38 +0900

    #99 OurPicks 페이지 컴포넌트 제작

---
+++ src/pages/WeLike/OurPicks/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const OurPicks = styled.div`
+  margin: 0 2rem;
+  h1 {
+    margin: 1.7rem 0 2.7rem;
+    font-family: 'Montserrat';
+    font-weight: 600;
+    font-size: 2.4rem;
+  }
+`;
