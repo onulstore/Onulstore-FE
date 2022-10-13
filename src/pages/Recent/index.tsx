1ab9ec04ec47e10d193a81c91c66d330c4476577
k-m-jin <kmjin1105@gmail.com>
2022-10-13 18:56:02 +0900

    #104 feat: getMyRecent 함수

--- src/pages/Recent/index.tsx
+++ src/pages/Recent/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import ItemAlbum from 'components/item/ItemAlbum';
+import api from 'utils/Api';
+import * as S from './style';
 import SetBottomTabBar from 'utils/HOC/SetBottomBar';
 import SetHeaderBar from 'utils/HOC/SetHeaderBar';
 
 function Recent() {
-  return <div>Recent</div>;
+  const getMyRecent = useCallback(async () => {
+    const res = await api({
+      url: 'members/latest',
+      method: 'GET',
+    });
+    console.log(res);
+  }, []);
+  useEffect(() => {
+    getMyRecent();
+  }, []);
+
+  return (
+    <div>
+      <S.TopBar>
+        <p className="total-count">총 11 개</p>
+      </S.TopBar>
+      {/* <ItemAlbum /> */}
+    </div>
+  );
 }
 
 export default SetHeaderBar(SetBottomTabBar(Recent));
--- src/pages/Recent/style.ts
+++ src/pages/Recent/style.ts
@@ -1,1 +1,24 @@
 import styled from 'styled-components';
+export const TopBar = styled.div`
+  height: 5.1rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0.8rem 2.4rem;
+  gap: 16.2rem;
+
+  border-bottom: 1px solid #d6d6d6;
+  box-sizing: border-box;
+
+  .total-count {
+    font-size: 1.4rem;
+    color: #505050;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    line-height: 1.5;
+    font-weight: 400;
+  }
+`;
