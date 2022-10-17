9147f772700fe5c5546cbbd8596ccc8196325181
Hwa-J <hj1103.code@gmail.com>
2022-10-17 16:47:47 +0900

    #123 deploy: redux-persist 세팅

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './slices/rootReducer';
+import { persistStore } from 'redux-persist';
 const store = configureStore({
   reducer: rootReducer,
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
