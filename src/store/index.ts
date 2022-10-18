ef51589ff5592507770be2b9b056c5035d095ef7
Hwa-J <hj1103.code@gmail.com>
2022-10-19 04:11:47 +0900

    #123 refactor: 직렬화 불가능값 오류 해결위한 middleware 옵션 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,12 @@ import rootReducer from './slices/rootReducer';
 import { persistStore } from 'redux-persist';
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST'],
+      },
+    }),
 });
 
 export type AppDispatch = typeof store.dispatch;
