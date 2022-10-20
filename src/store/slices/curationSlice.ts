2fae4d542938fc44b38ccce96b74c5450a1bf60e
Hwa-J <hj1103.code@gmail.com>
2022-10-20 22:30:03 +0900

    #125 feat: Home page용 magazine, recommend filter 기능 curationsSlice action 구현

--- src/store/slices/curationSlice.ts
+++ src/store/slices/curationSlice.ts
@@ -1,18 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { getCurations } from 'utils/Api/curationApi';
+import { getCurations, getMagazine, getRecommend } from 'utils/Api/curationApi';
+
+interface Curation {
+  display: boolean;
+  curationForm: string;
+}
 
 interface StateType {
   curationsData: any;
+  magazineInHomePage: any;
+  recommendInHomePage: any;
 }
 
 const initialState: StateType = {
-  curationsData: {},
+  curationsData: [],
+  magazineInHomePage: [],
+  recommendInHomePage: [],
 };
 
 const curationsSlice = createSlice({
   name: 'curationSlice',
   initialState,
-  reducers: {},
+  reducers: {
+    displayMagazine: (state) => {
+      state.magazineInHomePage = state.curationsData.filter(
+        (curation: Curation) => curation.display === true && curation.curationForm === 'MAGAZINE',
+      );
+    },
+    displayRecommend: (state) => {
+      state.recommendInHomePage = state.curationsData.filter(
+        (curation: Curation) => curation.display === true && curation.curationForm === 'RECOMMEND',
+      );
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(getCurations.fulfilled, (state, { payload }) => {
       state.curationsData = payload;
@@ -21,3 +41,4 @@ const curationsSlice = createSlice({
 });
 
 export default curationsSlice.reducer;
+export const { displayMagazine, displayRecommend } = curationsSlice.actions;
