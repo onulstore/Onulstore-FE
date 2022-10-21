540fa28d2dc80b76c8d5ebfec25ee04b8eda7a45
Hwa-J <hj1103.code@gmail.com>
2022-10-21 23:26:10 +0900

    #128 refactor: search Api, Slice 호출 및 반환값 연결

--- src/components/search/SearchResults/index.tsx
+++ src/components/search/SearchResults/index.tsx
@@ -1,24 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './style';
-// import ItemFilterBar from 'components/item/ItemFilterBar';
 import ItemAlbum from 'components/item/ItemAlbum';
 import { useSearchResult } from '../hooks';
+import { useAppDispatch, useSearchSlice } from 'store/hooks';
+import { getSearch } from 'utils/Api/searchApi';
 
-function SearchResults({ searchedItemList }: any) {
+function SearchResults() {
   const searchResult = useSearchResult();
-  const [isShowFilterSelectBox, setIsShowFilterSelectBox] = useState(false);
+  const dispatch = useAppDispatch();
+  const { searchData } = useSearchSlice();
+  useEffect(() => {
+    dispatch(getSearch(searchResult));
+  }, [searchResult]);
   return (
     <S.SearchResults>
       <div className="search-keyword">
         <span>{searchResult}</span> 에 대한 검색결과 입니다.
       </div>
-      {/* <ItemFilterBar
-        filterSeletBoxHandler={setIsShowFilterSelectBox}
-        isShowFilterSelectBox={isShowFilterSelectBox}
-      /> */}
       <S.ItemBoxCopy>
-        {searchedItemList?.length > 0 ? (
-          searchedItemList.map((item: any, index: number) => {
+        {searchData?.length > 0 ? (
+          searchData.map((item: any, index: number) => {
             return <ItemAlbum key={item.id} item={item} index={index} />;
           })
         ) : (
